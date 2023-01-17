5924f9c410769b369e1817ce0dad1fe74d2b91cf
Loukei <weber781015+bitbucket@gmail.com>
2023-01-17 16:37:57 +0800

    add serilog

--- src/EC2/Program.cs
+++ src/EC2/Program.cs
@@ -2,37 +2,65 @@ using EC2.Context;
 using EC2.Repository;
 using EC2.Service;
 using EC2.Service.Implement;
+/// add Serilog addon
+using Serilog;
+using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+/// Setting Serilog Logger
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-// Dapper
-builder.Services.AddSingleton<DapperContext>();
-// Register ProductRepository to services
-builder.Services.AddSingleton<IProductRepository, ProductRepository>();
-builder.Services.AddSingleton<ISuppilierRepository, SuppilierRepository>();
-builder.Services.AddSingleton<ICategoryRepository, CategoryRepository>();
-builder.Services.AddSingleton<IProductService, ProductService>();
+builder.Host.UseSerilog();
 
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
+    
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    Log.Information("Start Logging...");
+    /// Put original program.cs code here
+
+    // Add services to the container.
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    // Dapper
+    builder.Services.AddSingleton<DapperContext>();
+
+    // Register ProductRepository to services
+    builder.Services.AddSingleton<IProductRepository, ProductRepository>();
+    builder.Services.AddSingleton<ISuppilierRepository, SuppilierRepository>();
+    builder.Services.AddSingleton<ICategoryRepository, CategoryRepository>();
+    builder.Services.AddSingleton<IProductService, ProductService>();
+
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+}
+catch (Exception e)
+{
+    Log.Fatal(e, "Host terminated unexpectedly.");
+}
+finally
+{
+    Log.CloseAndFlush();
 }
 
-app.UseHttpsRedirection();
 
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
